d14c6cee363f7360c9ff4552802f30b3dc812e70
Josep Cugat <josep.cugat@skyscanner.net>
2020-12-08 12:42:33 +0100

    [day01] [star2] Solved

--- src/day01.rs
+++ src/day01.rs
@@ -11,6 +11,16 @@ pub fn star_one(input: &str) -> i64 {
 }
 
 pub fn star_two(input: &str) -> i64 {
+    let numbers: Vec<i64> = input.lines().map(|x| x.parse::<i64>().unwrap()).collect();
+    for i in 0..numbers.len() {
+        for j in (i + 1)..numbers.len() {
+            for k in (j + 1)..numbers.len() {
+                if numbers[i] + numbers[j] + numbers[k] == 2020 {
+                    return numbers[i] * numbers[j] * numbers[k];
+                }
+            }
+        }
+    }
     0
 }
 
@@ -25,6 +35,6 @@ mod tests {
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two("1721\n979\n366\n299\n675\n1456"), 241861950)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,6 @@ mod tests {
         let input = load_file("day01.txt");
 
         assert_eq!(star_one(&input), 445536);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_two(&input), 138688160);
     }
 }
