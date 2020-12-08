03d583b48c1e697a9754a5fcff030e38f1f6e3ce
Josep Cugat <josep.cugat@skyscanner.net>
2020-12-08 16:53:27 +0100

    [day02] Extract logic to filter function

--- src/day02.rs
+++ src/day02.rs
@@ -1,70 +1,66 @@
 use crate::utils::load_file;
 use std::{ops::RangeInclusive, path::Path};
 
-pub fn star_one(input: &str) -> i64 {
-    let lines: Vec<&str> = input.lines().collect();
-    let mut valid = 0;
-    for line in lines {
-        // Format is:
-        //    N-M C: abcde
-        //     ^ ^ ^
-        //     | | |
-        // idx 1 2 3
-        // N: min count
-        // M: max count
-        // C: character to check
-        let idx1 = line.find('-').unwrap();
-        let idx2 = line.find(' ').unwrap();
-        let idx3 = line.find(':').unwrap();
+fn is_valid_one(line: &str) -> bool {
+    // Format is:
+    //    N-M C: abcde
+    //     ^ ^ ^
+    //     | | |
+    // idx 1 2 3
+    // N: min count
+    // M: max count
+    // C: character to check
+    let idx1 = line.find('-').unwrap();
+    let idx2 = line.find(' ').unwrap();
+    let idx3 = line.find(':').unwrap();
 
-        let range = RangeInclusive::new(
-            line[0..idx1].parse().unwrap(),
-            line[idx1 + 1..idx2].parse().unwrap(),
-        );
-        let char_to_check: char = line[idx2 + 1..idx2 + 2].parse().unwrap();
-        let password = line[idx3 + 2..].to_string();
+    let range = RangeInclusive::new(
+        line[0..idx1].parse().unwrap(),
+        line[idx1 + 1..idx2].parse().unwrap(),
+    );
+    let char_to_check: char = line[idx2 + 1..idx2 + 2].parse().unwrap();
+    let password = line[idx3 + 2..].to_string();
 
-        let occurrences = password.chars().filter(|x| x == &char_to_check).count();
-        if range.contains(&occurrences) {
-            valid += 1;
-        }
-    }
-    valid
+    let occurrences = password.chars().filter(|x| x == &char_to_check).count();
+    range.contains(&occurrences)
 }
 
-pub fn star_two(input: &str) -> i64 {
-    let lines: Vec<&str> = input.lines().collect();
-    let mut valid = 0;
-    for line in lines {
-        // Format is:
-        //    N-M C: abcde
-        //     ^ ^ ^
-        //     | | |
-        // idx 1 2 3
-        // N: first position to check
-        // M: second position to check
-        // C: character to check
-        let idx1 = line.find('-').unwrap();
-        let idx2 = line.find(' ').unwrap();
-        let idx3 = line.find(':').unwrap();
+fn is_valid_two(line: &str) -> bool {
+    // Format is:
+    //    N-M C: abcde
+    //     ^ ^ ^
+    //     | | |
+    // idx 1 2 3
+    // N: first position to check
+    // M: second position to check
+    // C: character to check
+    let idx1 = line.find('-').unwrap();
+    let idx2 = line.find(' ').unwrap();
+    let idx3 = line.find(':').unwrap();
 
-        let pos1: usize = line[0..idx1].parse().unwrap();
-        let pos2: usize = line[idx1 + 1..idx2].parse().unwrap();
-        let char_to_check: char = line[idx2 + 1..idx2 + 2].parse().unwrap();
-        let password = line[idx3 + 2..].to_string();
+    let pos1: usize = line[0..idx1].parse().unwrap();
+    let pos2: usize = line[idx1 + 1..idx2].parse().unwrap();
+    let char_to_check: char = line[idx2 + 1..idx2 + 2].parse().unwrap();
+    let password = line[idx3 + 2..].to_string();
 
-        let mut matches = 0;
-        if password.chars().nth(pos1 - 1).unwrap() == char_to_check {
-            matches += 1
-        }
-        if password.chars().nth(pos2 - 1).unwrap() == char_to_check {
-            matches += 1
-        }
-        if matches == 1 {
-            valid += 1
-        }
+    let mut matches = 0;
+    if password.chars().nth(pos1 - 1).unwrap() == char_to_check {
+        matches += 1
     }
-    valid
+    if password.chars().nth(pos2 - 1).unwrap() == char_to_check {
+        matches += 1
+    }
+    matches == 1
+}
+
+pub fn star_one(input: &str) -> usize {
+    let lines: Vec<&str> = input.lines().collect();
+    lines.iter().filter(|x| is_valid_one(x)).count()
+}
+
+pub fn star_two(input: &str) -> usize {
+    let lines: Vec<&str> = input.lines().collect();
+    lines.iter().filter(|x| is_valid_two(x)).count()
 }
 
 #[cfg(test)]
