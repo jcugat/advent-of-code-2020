0516657e06b7750f2c83af625712a65e034ea32d
Josep Cugat <josep.cugat@skyscanner.net>
2020-12-08 12:02:24 +0100

    Structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+#![allow(dead_code)]
+mod day01;
+
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+    use std::io::Read;
+
+    fn load_file(path: &str) -> String {
+        let mut input = String::new();
+        let mut f = File::open(path).expect("Unable to open file");
+        f.read_to_string(&mut input).expect("Unable to read string");
+
+        input
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn solve_day01() {
+        use crate::day01::{star_one, star_two};
+
+        let input = load_file("day01.txt");
+
+        assert_eq!(star_one(&input), 1);
+        assert_eq!(star_two(&input), 1);
     }
 }
