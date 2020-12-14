63b3f694680093c7328af6cd444f3e06c6a8c702
Josep Cugat <josep.cugat@skyscanner.net>
2020-12-15 00:21:52 +0100

    [day07] [star2] Solved

--- src/day07.rs
+++ src/day07.rs
@@ -39,8 +39,79 @@ pub fn star_one(input: &str) -> usize {
     visited.len() - 1
 }
 
+struct Bag {
+    name: String,
+    contents: Vec<BagContent>,
+}
+impl Bag {
+    fn new(name: String) -> Bag {
+        Bag {
+            name,
+            contents: Default::default(),
+        }
+    }
+}
+
+struct BagContent {
+    quantity: i64,
+    bag: Bag,
+}
+impl BagContent {
+    fn new(quantity: i64, name: String) -> BagContent {
+        BagContent {
+            quantity,
+            bag: Bag::new(name),
+        }
+    }
+}
+
 pub fn star_two(input: &str) -> i64 {
-    0
+    let mut rules: Vec<Bag> = Default::default();
+
+    for line in input.lines() {
+        let mut iter1 = line.split(" bags contain ");
+        let bag_name = iter1.next().unwrap().to_owned();
+        let bag_contents = iter1.next().unwrap().strip_suffix('.').unwrap();
+
+        let mut bag = Bag::new(bag_name);
+
+        if bag_contents == "no other bags" {
+            rules.push(bag);
+            continue;
+        }
+
+        for bag_content in bag_contents.split(", ") {
+            let num_and_bag = bag_content
+                .strip_suffix(" bags")
+                .unwrap_or_else(|| bag_content.strip_suffix(" bag").unwrap());
+            let mut iter2 = num_and_bag.splitn(2, ' ');
+            let inner_bag_quantity = iter2.next().unwrap().parse::<i64>().unwrap();
+            let inner_bag_name = iter2.next().unwrap().to_owned();
+
+            bag.contents
+                .push(BagContent::new(inner_bag_quantity, inner_bag_name));
+        }
+        rules.push(bag);
+    }
+    search(&rules, "shiny gold") - 1
+}
+
+fn search(rules: &[Bag], current_name: &str) -> i64 {
+    for bag in rules {
+        if bag.name == current_name {
+            if bag.contents.is_empty() {
+                return 1;
+            }
+
+            let mut total_inner = 0;
+            for inner_bag in bag.contents.iter() {
+                let count_inner = search(rules, &inner_bag.bag.name);
+                total_inner += count_inner * inner_bag.quantity;
+            }
+            return total_inner + 1;
+        }
+    }
+    panic!("Could not find bag with name {}", current_name);
 }
 
 #[cfg(test)]
@@ -54,7 +125,7 @@ mod tests {
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(&get_input()), 1)
+        assert_eq!(star_two(&get_input()), 32)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,6 @@ mod tests {
         let input = load_file(&file_name);
 
         assert_eq!(star_one(&input), 265);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_two(&input), 14177);
     }
 }
